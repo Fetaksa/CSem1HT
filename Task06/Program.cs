df15810dd690166343ec1af764bc0f1e7b71de11
Fetaksa <rustykami@yandex.ru>
2022-10-15 19:08:58 +0300

    Task06 completed

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным.
+
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+
+else
+{
+    Console.WriteLine("Это НЕчетное число");
+}
