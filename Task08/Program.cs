5bda8db6d2bb9a89b5c4584cad7eed82e38f15a4
Fetaksa <rustykami@yandex.ru>
2022-10-15 20:00:09 +0300

    Task08 completed

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на входе принимает число (N),
+// а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i <= N; i++)
+{if (i % 2 == 0)
+{
+    Console.WriteLine(i);
+} 
+}
